04bbc1c096b0f85aa9c7d3a63deb45c1b6d65ebc
sudheer <sudheerlikeu@gmail.com>
2022-08-13 18:56:47 +0530

    --- Auto Git Commit ---

--- linux/secret.sh
+++ linux/secret.sh
@@ -5,4 +5,4 @@ echo "==================================="
 read -p "Enter Your Name :" Name 
 read -p "Enter Your favourite Actor :" Actor
 read -p "Enter Your Lucky Number :" lucky
-read -p "Enter Your favourite Din :" Name
+read -p "Enter Your favourite Dish :" dish
