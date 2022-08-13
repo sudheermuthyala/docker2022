8afcfee3881cc235e7690ba8cb0250c9719a8916
sudheer <sudheerlikeu@gmail.com>
2022-08-13 19:54:55 +0530

    --- Auto Git Commit ---

--- linux/secret.sh
+++ linux/secret.sh
@@ -7,3 +7,4 @@ read -p "Enter Your favourite Actor :" Actor
 read -p "Enter Your Lucky Number :" lucky
 read -p "Enter Your favourite Dish :" dish
 first_char_name=$(echo -n $Name | cut -c 1)
+len=$(echo -n $Actor | wc -c)
